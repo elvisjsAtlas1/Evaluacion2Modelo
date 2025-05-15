31f01d2bdec167abc263158f973b57598eb66f03
elvisjsAtlas1 <chat02jselvis@gmai.com>
2025-05-14 22:51:14 -0500

    actualizando

--- eja-auth/src/main/java/com/eja/auth/EjaAuthApplication.java
+++ eja-auth/src/main/java/com/eja/auth/EjaAuthApplication.java
@@ -2,10 +2,9 @@ package com.eja.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+
 public class EjaAuthApplication {
 
     public static void main(String[] args) {
