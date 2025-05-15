16a003e1d4e7352bee413ddeaaacf226084dfadf
ElvisjsAtlas <elvisjesusapazayucra15@gmail.com>
2025-05-15 07:57:01 -0500

    corregido

--- eja-auth/src/main/java/com/eja/auth/entity/AuthUser.java
+++ eja-auth/src/main/java/com/eja/auth/entity/AuthUser.java
@@ -2,16 +2,16 @@ package com.eja.auth.entity;
 
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
