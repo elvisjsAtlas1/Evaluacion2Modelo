6826c38776e8e3808a92731d29b16d8b6fcd50ca
elvisjsAtlas1 <chat02jselvis@gmail.com>
2025-05-21 13:34:22 -0500

    seguridad agregando

---
+++ ms-gateway-server/src/main/java/com/eja/gateway/dto/TokenDto.java
@@ -0,0 +1,19 @@
+package com.eja.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+
+@NoArgsConstructor
+
+
+@Builder
+@AllArgsConstructor
+@Data
+public class TokenDto {
+    private String token;
+}
