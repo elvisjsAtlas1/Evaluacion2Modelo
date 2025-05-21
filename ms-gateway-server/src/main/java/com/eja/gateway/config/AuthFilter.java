a0a92b01c5e98a59eaada79275f868ad30e04823
elvisjsAtlas1 <chat02jselvis@gmai.com>
2025-05-21 16:46:57 -0500

    actualizando direccion authFilter

--- ms-gateway-server/src/main/java/com/eja/gateway/config/AuthFilter.java
+++ ms-gateway-server/src/main/java/com/eja/gateway/config/AuthFilter.java
@@ -32,7 +32,7 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
                 return onError(exchange, HttpStatus.BAD_REQUEST);
             return webClient.build()
                     .post()
-                    .uri("http://ms-auth-service/auth/validate?token=" + chunks[1])
+                    .uri("http://eja-auth-service/auth/validate?token=" + chunks[1])
                     .retrieve().bodyToMono(TokenDto.class)
                     .map(t -> {
                         t.getToken();
